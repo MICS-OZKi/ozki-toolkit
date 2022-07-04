77e329997a5b9fc62e3e00dfcbeae9cb19b8fd0d
0xA-ha <a-ha@berkeley.edu>
2022-07-03 17:06:45 -0700

    completed ozki-lib interface

--- proof-generator/src/ProofGenerator.ts
+++ proof-generator/src/ProofGenerator.ts
@@ -19,39 +19,44 @@ export default abstract class ProofGenerator<Type> {
   // to format caller-specific input parameters
   protected abstract formatCustomInput(customInput: Type): any  
 
+  protected formatRequiredInput(signature: Uint8Array, timeStamp: number): any {
+    const zkutils = new ZkUtils();
+    return { 
+      sigR8: zkutils.buffer2bits(signature.slice(0, 32)),
+      sigS: zkutils.buffer2bits(signature.slice(32, 64)),
+      ts: zkutils.numberToBytes(timeStamp, 4) // timestamp (4 bytes)
+    }
+  }
+
   generateProof = async (
-    pSignature: Uint8Array,
-    timestamp: number,
+    signature: Uint8Array,
+    timeStamp: number,
     customInput: Type
   ): Promise<[string, string]> => {
+    console.log("#### >>ProofGenerator.generatorProof");
     // form the path for the wasm and proving key files
     const wasmFile = `${this.zkpComponentPath}${this.zkpComponentName}.wasm`;
     const provingKeyFile = `${this.zkpComponentPath}${this.zkpComponentName}.zkey`;
 
     // format the incoming params for circom input signals
-    const zkutils = new ZkUtils();
-    const r8Bits = zkutils.buffer2bits(pSignature.slice(0, 32));
-    const sBits = zkutils.buffer2bits(pSignature.slice(32, 64));
     let input = {
-      ...this.formatCustomInput(customInput), // caller-specific input
-      sigR8: r8Bits,  // dsa
-      sigS: sBits,    // dsa
-      ts: zkutils.numberToBytes(timestamp, 4), // timestamp (4 bytes)
+      ...this.formatRequiredInput(signature, timeStamp),
+      ...this.formatCustomInput(customInput) // caller-specific input
     };
 
     try {
       const t1 = new Date().getTime();
       // create the zk-snark proof
-      const { proof, publicSignals } = await snarkjs.groth16.fullProve(
+      const {proof, publicSignals} = await snarkjs.groth16.fullProve(
         input,
         wasmFile,
         provingKeyFile
       )
       const t2 = new Date().getTime();
-      console.log("#### proof generation took %d ms", t2-t1);
-
+      console.log("#### <<ProofGenerator.generatorProof: elapsedTime=%d", t2-t1);
       return [proof, publicSignals];
-    } catch (error: any) {
+    } 
+    catch (error: any) {
       console.log(error);
       return ["", ""];
     }
